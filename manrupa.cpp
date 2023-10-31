45b60f296858f593b417a9daf0b0381c98167318
Kripa1729 <93019841+Kripa1729@users.noreply.github.com>
2023-10-31 20:46:25 +1100

    Add files via upload

---
+++ manrupa.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+void writing() {
+  ofstream fout;
+  fout.open("file.txt", ios::out);
+  char string[] = "Mero pyaro mamu";
+  fout << string;
+  fout.close();
+}
+void reading() {
+  ifstream fin;
+  fin.open("file.txt", ios::in);
+  if (!fin) {
+    cerr << "error in opeinig the file";
+  } else {
+    string word;
+    while (!fin.eof()) {
+      fin >> word;
+      cout << word;
+    }
+  }
+  fin.close();
+}
+void append(char data[]) {
+  ofstream fout;
+  fout.open("file.txt", ios::app);
+  fout<<data;
+  fout.close();
+}
+int main() {
+  writing();
+  reading();
+  char string[] = "mero pyaro baba,babu,aama,mamamaiju";
+  append(string);
+  reading();
+}
